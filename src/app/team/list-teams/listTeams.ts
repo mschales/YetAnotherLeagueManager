af8555e51d7252ddbef6b76a50f2c214167df9b0
mschales <mich.schales@gmail.com>
2019-09-29 12:54:34 +0300

    Added "add player to team" feature including list view of players

--- src/app/team/list-teams/listTeams.ts
+++ src/app/team/list-teams/listTeams.ts
@@ -2,4 +2,4 @@ export function getTeam ()  {
     const teamsObject = this.parse.Object.extend("teams");
     const getTeam = new this.parse.Query(teamsObject);
     return getTeam.get.length;
-  }
+}
--- src/app/team/view-team/view-team.component.ts
+++ src/app/team/view-team/view-team.component.ts
@@ -11,6 +11,12 @@ import { ParseService } from '../../parse.service';
 export class ViewTeamComponent implements OnInit {
 
   parse: any;
+  playerList: any;
+  firstname: string;
+  surname: string;
+  jersey: number;
+  position: string;
+  teamid: number;
   teamName: string;
   authLevel: number;
   id: string;
@@ -39,11 +45,63 @@ export class ViewTeamComponent implements OnInit {
       })
   };
 
+  getPlayerList = () => {
+    const newTeam = this.parse.Object.extend("Players");
+    const query = new this.parse.Query(newTeam);
+    const tempList = [];
+    const getPlayers = () => {
+      query.find().then(res => {
+        res.forEach((item) => {
+          tempList.push({
+            firstname: item.get("firstname"),
+            surname: item.get('surname'),
+            position: item.get('position'),
+            jersey: item.get('jersey'),
+          })
+        })
+      })
+      return tempList;
+    }
+    this.playerList = getPlayers();
+  }
+
+  updateFirstName = (event: KeyboardEvent) => {
+    this.firstname = (<HTMLInputElement>event.target).value;
+  }
+
+  updateSurname = (event: KeyboardEvent) => {
+    this.surname = (<HTMLInputElement>event.target).value;
+  }
+
+  updatePosition = () => {
+    this.position = (<HTMLInputElement>event.target).value;
+  }
+
+  updateJersey = () => {
+    this.jersey = parseInt((<HTMLInputElement>event.target).value);
+  }
+
+  addPlayerSubmit = () => {
+    const playerObject = this.parse.Object.extend('Players');
+    const addPlayer = new playerObject();
+    addPlayer.set('firstname', this.firstname);
+    addPlayer.set('surname', this.surname);
+    addPlayer.set('position', this.position);
+    addPlayer.set('teamid', this.id);
+    addPlayer.save().then((player) => {
+      alert('Added player to team')
+    }, (error) => { console.log(error.message) }
+    )
+  }
+
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.id = params['id'];
     });
     this.getUser();
     this.getTeam();
+    this.getPlayerList();
+    console.log(this.playerList);
   }
 }
+
