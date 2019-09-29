1c8a8b91a0337f391a3aebd7bee41c24a15e1afc
mschales <mich.schales@gmail.com>
2019-09-29 11:14:42 +0300

    CSS tweaks for home and team pages

--- src/app/team/view-team/view-team.component.ts
+++ src/app/team/view-team/view-team.component.ts
@@ -1,23 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { ParseService } from '../../parse.service';
 
 @Component({
   selector: 'app-view-team',
   templateUrl: './view-team.component.html',
   styleUrls: ['../team.css']
 })
+
 export class ViewTeamComponent implements OnInit {
 
+  parse: any;
+  teamName: string;
+  authLevel: number;
   id: string;
-  private sub: any; 
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, parseService: ParseService) {
+    this.parse = parseService.getParse();
   }
+
+  getUser = () => {
+    const user = this.parse.User.current();
+    const userData = this.parse.Object.extend("User")
+    const query = new this.parse.Query(userData);
+    return query.get(user.id).then(
+      (user) => {
+        this.authLevel = user.get("role");
+      }
+    );
+  }
+
+  getTeam = () => {
+    const parseTeam = this.parse.Object.extend("teams");
+    let query = new this.parse.Query(parseTeam);
+    query.get(this.id)
+      .then((item) => {
+        this.teamName = item.get("name");
+      })
+  };
 
   ngOnInit() {
-    this.route.params.subscribe(params => { 
-      this.id = params['id']; 
-  });  
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+    this.getUser();
+    this.getTeam();
   }
-
 }
